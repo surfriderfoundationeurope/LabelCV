87a7c5bd871f4d74843ae40a345ffb983c15bfc9
Christopher Maneu <christopher@maneu.net>
2019-07-24 14:57:44 +0000

    chore!: Get listening port from configurationBREAKING CHANGE: the listening port is no longer hardcodedto 3000, but rather loaded from configuration via `API_PORT` key.Fixes AB#10

--- api/labelcv-api/src/main.ts
+++ api/labelcv-api/src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
