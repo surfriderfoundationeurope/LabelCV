ceba29bb1de71e1390d134db27ae83f707e81b53
Christopher Maneu <christopher@maneu.net>
2019-07-24 15:10:47 +0000

    chore: add proper logging of app configuration loading

--- api/labelcv-api/src/main.ts
+++ api/labelcv-api/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+
+  const logger = new Logger('main');
+
   if (process.env.NODE_ENV !== 'production') {
-    dotenv.config();
+    const result = dotenv.config();
+    logger.log('Configuration loaded from .env.');
+    logger.log('Keys loaded from configuration: ' + Object.keys(result.parsed) );
   }
 
   const app = await NestFactory.create(AppModule);
