af1f36dc28d86007ebce2b213ea78c65f8070661
Christopher MANEU <christopher@maneu.net>
2019-08-14 09:16:57 +0000

    WIP: add file uplaod

--- api/labelcv-api/src/app.module.ts
+++ api/labelcv-api/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImageModule } from './images/image.module';
+import { AzureStorageModule } from '@nestjs/azure-storage';
 
 @Module({
-  imports: [ImageModule],
+  imports: [ 
+    ImageModule, 
+    AzureStorageModule.withConfig({sasKey: process.env['AZURE_STORAGE_SAS_KEY'], 
+    accountName: process.env['AZURE_STORAGE_ACCOUNT'], 
+    containerName: process.env['AZURE_STORAGE_CONTAINER_NAME'] })],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/labelcv-api/src/images/image.module.ts
+++ api/labelcv-api/src/images/image.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ImageController } from './image.controller';
 import { ImageRepository } from './image.repository';
 import { ImageService } from './image.service';
+import { AzureStorageModule } from '@nestjs/azure-storage';
 
 @Module({
-  imports: [],
+  imports: [
+    AzureStorageModule.withConfig({sasKey: process.env['AZURE_STORAGE_SAS_KEY'],
+    accountName: process.env['AZURE_STORAGE_ACCOUNT'],
+    containerName: process.env['AZURE_STORAGE_CONTAINER_NAME'] })
+  ],
   controllers: [ImageController],
   providers: [ImageRepository, ImageService],
 })
