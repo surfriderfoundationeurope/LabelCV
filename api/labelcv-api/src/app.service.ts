78e6040d908bce606f45a6cce562b28a026125f5
Christopher MANEU <chmaneu@microsoft.com>
2021-02-11 15:59:47 +0100

    Update app.service.ts
    I just want to confirm my CD/CI pipeline is working. It's too magic (so maybe it's not working at all :D )

--- api/labelcv-api/src/app.service.ts
+++ api/labelcv-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to LabelCV API.';
+    return 'Welcome to LabelCV API v1.';
   }
 }
